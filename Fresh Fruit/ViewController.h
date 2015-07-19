1e9c28dcc58e5acc625c471852cb3f2085e6e5c7
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2015-07-19 19:01:57 -0400

    Initial Commit

---
+++ Fresh Fruit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fresh Fruit
+//
+//  Created by Cheng Hann Gan on 7/19/15.
+//  Copyright (c) 2015 Fresh Fruit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Fresh Fruit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Fresh Fruit
+//
+//  Created by Cheng Hann Gan on 7/19/15.
+//  Copyright (c) 2015 Fresh Fruit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
